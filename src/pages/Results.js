162e32542d60ea01e72d7aea8b5269ba33703da3
hayleel <hayleel@iastate.edu>
2021-04-28 16:42:22 -0500

    fixed toggle why

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,31 +1,13 @@
 import Header from './Header'
 import DataTable from './Table'
 
-import { makeStyles, Button } from "@material-ui/core"
+import { makeStyles, withStyles, Button } from "@material-ui/core"
 import React from 'react'
 import QuestionBoxes from './QuestionBoxes';
 
 const useStyles = makeStyles(() => ({
-    whyButton: {
-      fontFamily: "Open Sans, sans-serif",
-      fontWeight: 700,
-      fontSize: "20px",
-      textTransform: "lowercase",
-    //   marginLeft: "45%",
-      color: "#FFFFEE",
-      backgroundColor: "#1E2124",
-      margin: "auto",
-    },
-    mainWords: {
-        fontFamily: "Open Sans, sans-serif",
-        fontWeight: 700,
-        fontSize: "40px",
-        margin: "25px",
-        textAlign: "center",
-        color: "#1E2124",
-    },
     container: {
-        backgroundColor: "#545F66",
+        // backgroundColor: "#545F66", // for testing/sizing
         marginLeft: 5,
         width: "99%",
         height: 570,
@@ -33,7 +15,7 @@ const useStyles = makeStyles(() => ({
         marginTop: 100,
     },
     content_container: {
-        backgroundColor: "green",
+        // backgroundColor: "green",  // for testing/sizing
         // flex: "auto",
         margin: 1,
         // width: "49.8%",
@@ -43,12 +25,33 @@ const useStyles = makeStyles(() => ({
     },
 }));
 
+const styles = theme => ({
+    whyButton: {
+        fontFamily: "Open Sans, sans-serif",
+        fontWeight: 700,
+        fontSize: "20px",
+        textTransform: "lowercase",
+      //   marginLeft: "45%",
+        color: "#FFFFEE",
+        backgroundColor: "#1E2124",
+        margin: "auto",
+      },
+      mainWords: {
+          fontFamily: "Open Sans, sans-serif",
+          fontWeight: 700,
+          fontSize: "40px",
+          margin: "25px",
+          textAlign: "center",
+          color: "#1E2124",
+      },
+});
+
 function ResultsTable() {
     const { container, content_container } = useStyles();
     return(
         <div className={container}>
             <div className={content_container} style={{ float: "left", width: "40%" }}>
-                <QuestionBoxes style={{fontSize: 5}}/>
+                <QuestionBoxes />
             </div>
             <div className={content_container} style={{float: "right", width: "59.5%" }}>
                 <DataTable />
@@ -60,35 +63,41 @@ function ResultsTable() {
 class Results extends React.Component {
     state = {
         isActive: false
+    };
+    
+    handleShow = () => {
+        this.setState({isActive: true});
       };
     
-      handleHide() {
-        if (this.state.isActive) {
-            this.setState({
-                isActive: false
-            });
-        }
-        else {
-            this.setState({
-                isActive: true
-            });
-        }
-    };
+      handleHide = () => {
+        this.setState({isActive: false});
+      };
 
     render () {
-        const { mainWords, whyButton } = useStyles();
-        return(
+        const { classes } = this.props;
+        return (
             <div>
                 <Header />
-                <h1 className={mainWords} style = {{marginTop: "15%"}}>Zoom is your best option</h1>
+                <h1 className={classes.mainWords} style = {{marginTop: "15%"}}>Zoom is your best option</h1>
                 <br></br>
-                <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleHide}/*color="primary"*/ className={whyButton} href="/results">
+                {/* <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleHide} className={classes.whyButton}>
                     why?
-                </Button></div>
-                <ResultsTable />
+                </Button></div> */}
+                
+                
+                {this.state.isActive ?(
+                    <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleHide} className={classes.whyButton}>
+                        why?
+                    </Button></div>
+                ) : (
+                    <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleShow} className={classes.whyButton}>
+                        why?
+                    </Button></div>
+                )}
+                {this.state.isActive && <ResultsTable />}
             </div>
         );
     }
 }
 
-export default Results;
+export default withStyles(styles)(Results);
