f52ee7a1d88a9ff179c1bec4f9df10eb71232e3f
hayleel <hayleel@iastate.edu>
2022-01-01 14:38:19 -0600

    change app.js test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,6 @@ import Home from '../pages/Home'
 
 test('renders learn react link', () => {
   render(<Home />);
-  const linkElement = screen.getByText(/should you/i);
+  const linkElement = screen.getByText(/should you travel for your meeting?/i);
   expect(linkElement).toBeInTheDocument();
 });
