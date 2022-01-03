88cae7881fadeda04041c9996191464e6a3c48a4
hayleel <hayleel@iastate.edu>
2022-01-03 12:31:12 -0600

    got button group to work + change table labels

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -7,7 +7,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
 
+/**
+ * the css styles for the components
+ */
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
@@ -21,13 +26,30 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(categories, rate, original) {
-  return { categories, rate, original };
+/**
+ * allows us to input values into the table
+ * @param {*} constructs the construct being evaluated 
+ * @param {*} degree whether the construct is needed to be low/med/high 
+ * @param {*} recommendation the type of meeting recommended based off of the degree and construct
+ * @returns the data row generated from the categories
+ */
+function createData(constructs, degree, recommendation) {
+  return { constructs, degree, recommendation };
 }
 
+/**
+ * creates a button group for low/med/high degree
+ */
+const buttonGroup = [
+  <ButtonGroup>
+    <Button>Low</Button>
+    <Button>Medium</Button>
+    <Button>High</Button>
+  </ButtonGroup>,
+];
+
 const rows = [
-  createData('Co-Presence', 15, 85),
-  createData('Engagement', 26, 10),
+  createData('Engagement', buttonGroup, 10),
   createData('Knowledge', 30, 37),
   createData('Mental Workload', 35, 100),
   createData('Performance', 67, 76),
@@ -45,19 +67,19 @@ function DataTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className={classes.tableCell}>Data Categories</TableCell>
-            <TableCell align="right" className={classes.tableCell}>Rate of Success</TableCell>
-            <TableCell align="right" className={classes.tableCell}>Your Success Rate</TableCell>
+            <TableCell className={classes.tableCell}>Constructs</TableCell>
+            <TableCell align="right" className={classes.tableCell}>Degree Required</TableCell>
+            <TableCell align="right" className={classes.tableCell}>Recommendation</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row" className={classes.tableCell}>
-                {row.categories}
+                {row.constructs}
               </TableCell>
-              <TableCell align="right" className={classes.tableCell}>{row.rate}%</TableCell>
-              <TableCell align="right" className={classes.tableCell}>{row.original}%</TableCell>
+              <TableCell align="right" className={classes.tableCell}>{row.degree}</TableCell>
+              <TableCell align="right" className={classes.tableCell}>{row.recommendation}</TableCell>
             </TableRow>
           ))}
         </TableBody>
