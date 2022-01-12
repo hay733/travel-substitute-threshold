434347985d5deb20e8b9576d828d23086b7c19f1
Natalie Lloyd <55217875+nlloyd1@users.noreply.github.com>
2022-01-11 20:22:55 -0600

    Add files via upload

--- src/Results.js
+++ src/Results.js
@@ -2,16 +2,11 @@ import Header from './Header'
 import DataTable from './Table'
 
 import { makeStyles, withStyles, Button } from "@material-ui/core"
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 import QuestionBoxes from './QuestionBoxes';
-import Modal from '@mui/material/Modal';
-import Box from "@material-ui/core/Box";
+import { ButtonGroup } from '@mui/material';
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import Slide from "@material-ui/core/Slide";
 
 const useStyles = makeStyles(() => ({
     container: {
@@ -54,85 +49,99 @@ const styles = theme => ({
           color: "#1E2124",
       },
 });
+
 // function ResultsTable() {
 //     const { container, content_container } = useStyles();
 //     return(
 //         <div className={container}>
-//             <div className={content_container} style={{ float: "left", width: "40%", overflowY: "scroll"}}>
+//             {/* <div className={content_container} style={{ float: "left", width: "40%", overflowY: "scroll"}}>
 //                 <QuestionBoxes />
+//             </div> */}
+//             <div className={content_container} /**style={{ float: "right", width: "59.5%" }}**/>
+//                 <DataTable />
 //             </div>
-//             <div className={content_container} style={{ float: "right", width: "59.5%" }} onClick={this.openModal}>
-//                 <DataTable/>
-//             </div>
-//         </div> 
+//         </div>
 //     );
 // }
 
-// const { container, content_container } = useStyles();
 class Results extends React.Component {
+    // state, handleshow and handlehide toggle resultstable, but dont need atm
+    // state = {
+    //     isActive: false
+    // };
     state = {
-        isActive: false,
         isOpen: false,
-        title: null, 
+        title: null,
         description: null
-    };
-    handleShow = () => {
-        this.setState({isActive: true});
-    };
+    }
     
-    handleHide = () => {
-        this.setState({isActive: false});
-    };
+    // handleShow = () => {
+    //     this.setState({isActive: true});
+    //   };
+    
+    // handleHide = () => {
+    //     this.setState({isActive: false});
+    // };
 
-    openModal = (e, data) => {
+    openModal = (e) => {
         console.log(e);
         this.title = e.target.innerText;
         var str = e.target.innerHTML;
         str.replace("=", '');
         this.description = str.replace(/<[^>]+>/g, '').replace(this.title, '');
-        
         this.setState({isOpen: true});
     }
     closeModal = () => {
         this.setState({isOpen: false});
     }
+
     render () {
         const { classes } = this.props;
         return (
-            <>
-                <div>
-                    <Header />
-                    <h1 className={classes.mainWords} style = {{marginTop: "15%"}}>A teleconferencing software is your best option</h1>
-                    <br></br>
-                    
-                    {this.state.isActive ?(
-                        <div>
-                            <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleHide} className={classes.whyButton}>
-                                hide data
-                            </Button></div>
-                            <div>
-                            <div className={classes.content_container} style={{ float: "left", width: "40%", overflowY: "scroll"}}>
-                                <QuestionBoxes />
-                            </div>
-                            <div className={classes.content_container} style={{ float: "right", width: "59.5%" }} onClick={this.openModal}>
-                                <DataTable/>
-                            </div>
-                        </div>
-                    </div>
-                    ) : (
-                        <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleShow} className={classes.whyButton}>
-                            data
-                        </Button></div>
-                    )}
-                    <Dialog open={this.state.isOpen} onClose={this.closeModal} maxWidth='md' fullWidth={true}>
-                        <Typography margin='10%' align='center'>
-                            <h2>{this.title}</h2>
-                            <p>{this.description}</p>
-                        </Typography>
-                        <Button onClick={this.closeModal}>Close</Button>
-                    </Dialog>
+            <div>
+                <Header />
+                <h1 className={classes.mainWords} style = {{marginTop: "15%"}}>A teleconferencing software is your best option</h1>
+                {/* <br></br> */}
+                
+                {/* {this.state.isActive ?(
+                    <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleHide} className={classes.whyButton}>
+                        hide data
+                    </Button></div>
+                ) : (
+                    <div style={{textAlign: "center"}}><Button variant="contained" onClick={this.handleShow} className={classes.whyButton}>
+                        data
+                    </Button></div>
+                )}
+                {this.state.isActive && <ResultsTable />} */}
+
+                {/* <ResultsTable /> */}
+                <div className={classes.content_container} style={{ float: "right", width: "59.5%" }} onClick={this.openModal}>
+                    <DataTable/>
                 </div>
-          </>
+                <Dialog open={this.state.isOpen} onClose={this.closeModal} maxWidth='md' fullWidth={true}>
+                    <Typography margin='10%' align='center'>
+                        <h2>{this.title}</h2>
+                        <p>{this.description}</p>
+                    </Typography>
+                    <Button onClick={this.closeModal}>Close</Button>
+                </Dialog>
+                <br></br><br></br><br></br>
+                {/* <div style={{textAlign: "center", marginBottom: "5%"}}>
+                    <a href='/about' classname={classes.mainWords} style={{color: "black", fontSize: 20, fontFamily: "Open Sans, sans-serif"}}>
+                        more info
+                    </a>
+                    <br></br>
+                    <a href='/about' classname={classes.mainWords} style={{color: "black", fontSize: 20, fontFamily: "Open Sans, sans-serif"}}>
+                        contribute my data anonymously
+                    </a>
+                </div> */}
+                <div style={{textAlign: "center"}}>
+                    <ButtonGroup variant="outlined" aria-label="text button group">
+                        <Button href="/about">more info</Button>
+                        <Button>anonymously contribute my data</Button>
+                    </ButtonGroup>
+                </div>
+            </div>
         );
     }
 }
