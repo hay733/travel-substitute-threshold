647280c57a29c5e153810edf2cb3acdfb76767e0
hayleel <hayleel@iastate.edu>
2022-03-22 18:41:29 -0500

    added description and citations pages

---
+++ src/pages/informational pages/Citations.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+
+/**
+ * 
+ * @returns the page that displays all the citations referenced
+ * throughout the site
+ */
+function Citations() {
+    return(
+        <div>
+            <Header/>
+            <h1 style={{marginTop: '10%'}}>Citations</h1>
+        </div>
+    )
+}
+
+export default Citations;
